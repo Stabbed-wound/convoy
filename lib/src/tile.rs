ada2072d80525f2851154b04a0c22f13e26e0986
GavGav <github.com.easter342@passmail.net>
2025-03-25 21:55:04 +1030

    Improved Tile struct

--- lib/src/tile.rs
+++ lib/src/tile.rs
@@ -1,17 +1,35 @@
 use crate::pieces::Piece;
 
 #[derive(Copy, Clone, Debug, Default)]
+pub enum TileType {
+    #[default]
+    Regular,
+    Town,
+    Supply,
+}
+
+#[derive(Copy, Clone, Debug, Default)]
 pub struct Tile {
-    has_supplies: bool,
     piece: Option<Piece>,
+    r#type: TileType,
 }
 
 impl Tile {
-    #[must_use] pub const fn has_supplies(self) -> bool {
-        self.has_supplies
+    #[must_use]
+    pub const fn has_supplies(&self) -> bool {
+        match self.r#type {
+            TileType::Town | TileType::Supply => true,
+            TileType::Regular => false,
+        }
     }
-    
-    #[must_use] pub const fn piece(&self) -> Option<&Piece> {
+
+    #[must_use]
+    pub const fn gives_income(&self) -> bool {
+        matches!(self.r#type, TileType::Town)
+    }
+
+    #[must_use]
+    pub const fn piece(&self) -> Option<&Piece> {
         self.piece.as_ref()
     }
 }
