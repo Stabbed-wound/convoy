5571680c030d245a2f0b2b60e4fe187e66b60bd7
GavGav <github.com.easter342@passmail.net>
2025-03-29 14:47:07 +1030

    Added Artillery to PieceType

--- lib/src/pieces.rs
+++ lib/src/pieces.rs
@@ -1,6 +1,6 @@
 use crate::{board::Board, coord::Coord};
 use std::fmt::Debug;
-use std::ops::Deref;
+use std::ops::{Deref, RangeInclusive};
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub enum PieceColour {
@@ -20,6 +20,7 @@ impl PieceColour {
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub enum PieceType {
+    Artillery,
     Convoy,
     Infantry,
     Recon,
@@ -29,8 +30,9 @@ impl PieceType {
     #[must_use]
     pub const fn cost(&self) -> u8 {
         match self {
-            Self::Convoy => 2,
-            Self::Infantry | Self::Recon => 3,
+            Self::Convoy => 3,
+            Self::Infantry => 2,
+            Self::Recon | Self::Artillery => 4,
         }
     }
 
@@ -38,8 +40,17 @@ impl PieceType {
     pub const fn power(&self) -> u8 {
         match self {
             Self::Convoy => 0,
-            Self::Infantry => 2,
+            Self::Infantry | Self::Artillery => 2,
             Self::Recon => 1,
+        }
+    }
+
+    #[must_use]
+    pub const fn range(&self) -> RangeInclusive<u8> {
+        match self {
+            Self::Artillery => 2..=3,
+            Self::Convoy => 0..=0,
+            Self::Infantry | Self::Recon => 1..=1,
         }
     }
 
@@ -47,7 +58,7 @@ impl PieceType {
     pub const fn speed(&self) -> u8 {
         match self {
             Self::Convoy => 3,
-            Self::Infantry => 2,
+            Self::Infantry | Self::Artillery => 2,
             Self::Recon => 4,
         }
     }
