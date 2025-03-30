af6e9020940d9ad430f6993e5a6a70390ae85ab7
GavGav <github.com.easter342@passmail.net>
2025-03-30 12:13:08 +1030

    Implemented IntoIterator for Board

--- lib/src/board.rs
+++ lib/src/board.rs
@@ -1,15 +1,39 @@
-use std::array;
 use std::ops::{Index, IndexMut};
+use std::{array, slice};
 
-use crate::tile::{Tile, TileType};
 use crate::{
     constants::{BOARD_FILES, BOARD_RANKS},
     coord::Coord,
+    tile::{Tile, TileType},
 };
 
 #[derive(Clone, Debug)]
 pub struct Board {
     tiles: [[Tile; BOARD_FILES as usize]; BOARD_RANKS as usize],
+}
+
+impl Board {
+    /// Neighbours are orthogonal only
+    #[must_use]
+    pub fn get_neighbours(&self, Coord { rank, file }: Coord) -> Vec<Tile> {
+        [
+            Coord::new(rank + 1, file),
+            Coord::new(rank.wrapping_sub(1), file),
+            Coord::new(rank, file + 1),
+            Coord::new(rank, file.wrapping_sub(1)),
+        ]
+        .into_iter()
+        .filter_map(|coord| coord.map(|coord| self[coord]))
+        .collect()
+    }
+
+    pub fn iter(&self) -> slice::Iter<Tile> {
+        self.into_iter()
+    }
+
+    pub fn iter_mut(&mut self) -> slice::IterMut<Tile> {
+        self.into_iter()
+    }
 }
 
 impl Default for Board {
@@ -40,18 +64,20 @@ impl IndexMut<Coord> for Board {
     }
 }
 
-impl Board {
-    /// Neighbours are orthogonal only
-    #[must_use]
-    pub fn get_neighbours(&self, Coord { rank, file }: Coord) -> Vec<Tile> {
-        [
-            Coord::new(rank + 1, file),
-            Coord::new(rank.wrapping_sub(1), file),
-            Coord::new(rank, file + 1),
-            Coord::new(rank, file.wrapping_sub(1)),
-        ]
-        .into_iter()
-        .filter_map(|coord| coord.map(|coord| self[coord]))
-        .collect()
+impl<'a> IntoIterator for &'a Board {
+    type Item = &'a Tile;
+    type IntoIter = slice::Iter<'a, Tile>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.tiles.as_flattened().iter()
+    }
+}
+
+impl<'a> IntoIterator for &'a mut Board {
+    type Item = &'a mut Tile;
+    type IntoIter = slice::IterMut<'a, Tile>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.tiles.as_flattened_mut().iter_mut()
     }
 }
